83ee63819b1414a209f6ed8748d1ce3ec15971a9
Logan Fate <logan.m.fate@gmail.com>
2024-02-19 13:34:34 -0500

    Added video model

---
+++ app/models/video.py
@@ -0,0 +1,24 @@
+from .db import db, environment, SCHEMA
+
+class Video(db.Model):
+    __tablename__ = 'videos'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    genre = db.Column(db.String(50), nullable=False)
+    thumbnail_url = db.Column(db.String(255), nullable=False)
+    video_url = db.Column(db.String(255), nullable=False)  # Assuming video_file refers to a URL
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'genre': self.genre,
+            'thumbnail_url': self.thumbnail_url,
+            'video_url': self.video_url
+        }
