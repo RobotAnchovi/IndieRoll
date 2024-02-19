0a5a889462f7d97f60b8c8102943a8e12cb000c5
Logan Fate <logan.m.fate@gmail.com>
2024-02-19 14:49:23 -0500

    Fixed table bugs

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
 from .db import db
 from .user import User
+from .videos import Video
+from .reviews import Review
 from .db import environment, SCHEMA
---
+++ app/models/reviews.py
@@ -0,0 +1,22 @@
+from .db import db
+from sqlalchemy.schema import ForeignKey
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    video_id = db.Column(db.Integer, db.ForeignKey('videos.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    review_text = db.Column(db.Text, nullable=True)
+
+    user = db.relationship('User', backref=db.backref('reviews', lazy=True))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'video_id': self.video_id,
+            'user_id': self.user_id,
+            'rating': self.rating,
+            'review_text': self.review_text
+        }
