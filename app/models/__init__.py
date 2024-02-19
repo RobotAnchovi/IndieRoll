7c74b9b637cdd9f832a5831040ac3945111d764f
worldofcreatives <worldofcreatives.com@gmail.com>
2024-02-19 17:32:20 -0500

    Deleted versions files and created a new one and seeded all

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,4 +3,6 @@ from .user import User
 from .videos import Video
 from .reviews import Review
 from .watchlist import Watchlist
+from .video_content import VideoContent
+from .genres import Genre
 from .db import environment, SCHEMA
