a5a62eab74d16427c80899cb7b76b1f8b9a30fb0
Marisela Gomez <mariselaalejandragomez@gmail.com>
2024-02-19 16:53:36 -0800

    seeders for genres and users

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-
+from .genres import seed_genres, undo_genres
 from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
@@ -12,12 +12,14 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
+        undo_genres()  # Add undo_genres before seeding in production
     seed_users()
+    seed_genres()
     # Add other seed functions here
 
 
@@ -25,4 +27,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_genres()
     # Add other undo functions here
