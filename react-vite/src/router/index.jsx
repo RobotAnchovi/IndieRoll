12ca6d7883ee0864b75f413c457651efaae79917
Marisela Gomez <mariselaalejandragomez@gmail.com>
2024-02-20 12:59:03 -0800

    added landingpages

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,7 +1,8 @@
-import { createBrowserRouter } from 'react-router-dom';
-import LoginFormPage from '../components/LoginFormPage';
-import SignupFormPage from '../components/SignupFormPage';
-import Layout from './Layout';
+import { createBrowserRouter } from "react-router-dom";
+import LoginFormPage from "../components/LoginFormPage";
+import SignupFormPage from "../components/SignupFormPage";
+import LandingPage from "../components/LandingPage";
+import Layout from "./Layout";
 
 export const router = createBrowserRouter([
   {
@@ -9,7 +10,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Welcome!</h1>,
+        element: <LandingPage />,
       },
       {
         path: "login",
