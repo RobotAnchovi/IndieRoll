41e52fd439ab722245b3fc3df1aa8830f8aaade6
Logan Fate <logan.m.fate@gmail.com>
2024-02-20 20:25:10 -0500

    asd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # docker-entrypoint.sh
-
+DB_URL="postgres://app_academy_projects_7wv5_user:c2V7Ujo7yPB9kmfhQI8sjdNlaOUU39KJ@dpg-cn80s4q1hbls73d85k5g-a.ohio-postgres.render.com/app_academy_projects_7wv5"
 # Wait for the database to be available if necessary
 # Example using PostgreSQL as an example
 echo "Waiting for PostgreSQL to start..."
 # Wait for a few seconds or implement a loop checking db connection
+echo "Dropping alembic_version table..."
+psql $DB_URL -c "DROP TABLE IF EXISTS alembic_version;"
+
 
 echo "Removing existing migrations directory and SQLite database..."
 rm -rf /var/www/migrations /var/www/instance/dev.db
