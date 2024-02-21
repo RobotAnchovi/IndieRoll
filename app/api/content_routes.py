d3e07a66e96075537aa172b4ba9b243a2f4bd251
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-02-21 16:49:32 -0500

    Refactor content_routes.py and add content.js to redux

--- app/api/content_routes.py
+++ app/api/content_routes.py
@@ -51,55 +51,9 @@ def add_content():
             return jsonify({"error": "Could not add new content"}), 500
 
 
-    # data = request.get_json()
-    # title = data.get("title")
-    # description = data.get("description")
-    # genre = data.get("genre")
-    # thumbnail_url = data.get("thumbnail_url")
-    # video_url = data.get("video_file")
-
-    # existing_movie = VideoContent.query.filter(
-    #     (VideoContent.title == title) | (VideoContent.video_url == video_url)
-    # ).first()
-    # if existing_movie:
-    #     return jsonify({"error": "Movie already exists"}), 409
-
-    # if not title or not description or not genre or not thumbnail_url or not video_url:
-    #     return jsonify({"error": "Missing data for required fields"}), 400
-
-    # new_video = VideoContent(
-    #     title=title,
-    #     description=description,
-    #     genre=genre,
-    #     thumbnail_url=thumbnail_url,
-    #     video_url=video_url,
-    # )
-
-    # try:
-    #     db.session.add(new_video)
-    #     db.session.commit()
-    #     return jsonify(new_video.to_dict()), 201
-    # except IntegrityError:
-    #     db.session.rollback()
-    #     return jsonify({"error": "Could not add new content"}), 500
-
 
 @content_routes.route("/<int:contentId>", methods=["PUT"])
 @login_required
-# def update_content(contentId):
-    # if not current_user.is_creator:
-    #     return jsonify({"error": "You must be a creator to update content"}), 403
-
-    # video = VideoContent.query.get(contentId)
-    # if video is None:
-    #     return jsonify({"error": "Content not found"}), 404
-
-    # data = request.get_json()
-    # video.title = data.get("title", video.title)
-    # video.description = data.get("description", video.description)
-    # video.genre = data.get("genre", video.genre)
-    # db.session.commit()
-    # return jsonify(video.to_dict()), 200
 def update_content(video_id):
     if not current_user.is_creator:
         return jsonify({"error": "You must be a creator to update content"}), 403
@@ -140,17 +94,6 @@ def update_content(video_id):
 
 @content_routes.route("/<int:contentId>", methods=["DELETE"])
 @login_required
-# def delete_content(contentId):
-#     if not current_user.is_creator:
-#         return jsonify({"error": "You must be a creator to delete content"}), 403
-
-#     video = VideoContent.query.get(contentId)
-#     if video is None:
-#         return jsonify({"error": "Content not found"}), 404
-
-#     db.session.delete(video)
-#     db.session.commit()
-#     return "", 204
 def delete_content(video_id):
     if not current_user.is_creator:
         return jsonify({"error": "You must be a creator to delete content"}), 403
