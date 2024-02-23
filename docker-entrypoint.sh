d41284874d6af3c81261c70dcda89e38223e02f3
Logan Fate <logan.m.fate@gmail.com>
2024-02-23 13:05:21 -0500

    ReRan migrations and seeds

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,12 +6,14 @@
 echo "Waiting for PostgreSQL to start..."
 # Wait for a few seconds or implement a loop checking db connection
 
-# Run database migrations
+
 echo "Generating Database Tables..."
+flask db migrate
+
+# Run database migrations
+echo "Migrating Database..."
 flask db upgrade
 
-echo "Migrating Database..."
-flask db migrate
 
 # Seed the database if needed
 echo "Seeding the database..."
