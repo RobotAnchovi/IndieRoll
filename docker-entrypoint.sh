80c28201e9ac1130fd41133cafca197ff8663d0a
Logan Fate <logan.m.fate@gmail.com>
2024-02-23 12:59:34 -0500

    Adding migrate to dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,11 @@ echo "Waiting for PostgreSQL to start..."
 # Wait for a few seconds or implement a loop checking db connection
 
 # Run database migrations
-echo "Running database migrations..."
+echo "Generating Database Tables..."
 flask db upgrade
+
+echo "Migrating Database..."
+flask db migrate
 
 # Seed the database if needed
 echo "Seeding the database..."
