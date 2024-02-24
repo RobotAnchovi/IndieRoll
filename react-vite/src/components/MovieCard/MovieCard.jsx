c4e33c50ff744645a4296c412264949343531d25
Logan Fate <logan.m.fate@gmail.com>
2024-02-23 20:29:08 -0500

    fixed movie id page url issue

--- react-vite/src/components/MovieCard/MovieCard.jsx
+++ react-vite/src/components/MovieCard/MovieCard.jsx
@@ -23,7 +23,7 @@ const MovieCard = ({ movie }) => {
   };
 
   const handleDetailsClick = () => {
-    navigate(`/content/${movie.id}`, { state: { movie } });
+    navigate(`/content/all/${movie.id}`, { state: { movie } });
   };
 
   return (
