ff01494900ebd802fd72f62ecaec6d0fbfc841b1
Logan Fate <logan.m.fate@gmail.com>
2024-02-24 05:11:18 -0500

    foreign key constraint preventing film deletion

--- app/models/watchlist.py
+++ app/models/watchlist.py
@@ -13,7 +13,7 @@ class Watchlist(db.Model):
     video_id = db.Column(db.Integer, ForeignKey(add_prefix_for_prod("video_content.id")), nullable=True)
 
     user_watchlists = db.relationship("User", backref=db.backref("watchlist_items", lazy=True))
-    video_content = db.relationship("VideoContent", back_populates="watchlists")
+    video_content = db.relationship("VideoContent", back_populates="watchlists", cascade="all, delete", passive_deletes=True)
 
     def to_dict(self):
         return {"id": self.id, "user_id": self.user_id, "video_id": self.video_id}
