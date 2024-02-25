c1cb7bfc65584b79e12dca5de058c172fd9ce8f1
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-02-25 11:53:27 -0500

    Update script and fix navigation rendering

--- react-vite/src/components/ContentPage/ContentPage.jsx
+++ react-vite/src/components/ContentPage/ContentPage.jsx
@@ -1,5 +1,5 @@
-import { useEffect ,useState } from 'react';
-import { useParams, Navigate, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams, Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContentById } from '../../redux/content';
 import FeaturedMovie from '../FeaturedMovie';
@@ -8,37 +8,33 @@ import ReviewsSection from '../ReviewsSection/ReviewsSection';
 const ContentPage = () => {
   let { id } = useParams();
   const dispatch = useDispatch();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-
-  const isAuthenticated = useSelector(state => state.session.user);
-  const loading = useSelector(state => state.content.loading);
-  const movie = useSelector(state => state.content.currentContent);
-  console.log("🚀 ~ ContentPage ~ movie:", movie)
+  const isAuthenticated = useSelector((state) => state.session.user);
+  const loading = useSelector((state) => state.content.loading);
+  const movie = useSelector((state) => state.content.currentContent);
+  console.log('🚀 ~ ContentPage ~ movie:', movie);
 
   const [triggerFetch, setTriggerFetch] = useState(false);
 
-
   useEffect(() => {
-
     if (isAuthenticated) {
       dispatch(fetchContentById(id));
     }
   }, [id, isAuthenticated, dispatch, triggerFetch]);
 
-
   useEffect(() => {
     return () => setTriggerFetch(false);
   }, [id]);
 
-  const isOwner = movie?.user_id === isAuthenticated?.id;
+  // const isOwner = movie?.user_id === isAuthenticated?.id;
 
   useEffect(() => {
     if (isAuthenticated) {
-        if (!movie || movie.id.toString() !== id) {
-          dispatch(fetchContentById(id));
-        }
+      if (!movie || movie.id.toString() !== id) {
+        dispatch(fetchContentById(id));
       }
+    }
   }, [id, movie, dispatch, isAuthenticated]);
 
   if (loading) {
@@ -47,7 +43,7 @@ const ContentPage = () => {
 
   // Redirect if not authenticated
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to='/login' replace />;
   }
 
   if (!loading && !movie) {
--- react-vite/src/router/Layout.jsx
+++ react-vite/src/router/Layout.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
-import { Outlet, useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { ModalProvider, Modal } from "../context/Modal";
-import { thunkAuthenticate } from "../redux/session";
-import Navigation from "../components/Navigation/Navigation";
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { ModalProvider, Modal } from '../context/Modal';
+import { thunkAuthenticate } from '../redux/session';
+import Navigation from '../components/Navigation/Navigation';
 
 export default function Layout() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   const location = useLocation();
-  const showNavigation = !location.pathname.endsWith("play");
+  const showNavigation = !location.pathname.endsWith('play');
 
   useEffect(() => {
     dispatch(thunkAuthenticate()).then(() => setIsLoaded(true));
@@ -18,7 +18,7 @@ export default function Layout() {
   return (
     <>
       <ModalProvider>
-        {showNavigation && <Navigation />} // Conditionally render Navigation
+        {showNavigation && <Navigation />}
         {isLoaded && <Outlet />}
         <Modal />
       </ModalProvider>
