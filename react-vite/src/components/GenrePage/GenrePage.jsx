3f620c6626eed823696ff05e9f87af4d5451b540
Logan Fate <logan.m.fate@gmail.com>
2024-02-26 09:01:52 -0500

    Capitilized first letter of genre on genre page header

--- react-vite/src/components/GenrePage/GenrePage.jsx
+++ react-vite/src/components/GenrePage/GenrePage.jsx
@@ -14,9 +14,11 @@ const GenrePage = () => {
       dispatch(fetchContentByGenre(genreName));
     }, [dispatch, genreName]);
 
+    const displayGenreName = capitalizeFirstLetter(genreName.replace(/-/g, ' '));
+
     return (
       <div>
-         <h2>{genreName.replace(/-/g, ' ')} Movies</h2>
+          <h2>{displayGenreName} Movies</h2>
          <div>
           {movies.map(movie => (
             <MovieCard key={movie.id} movie={movie} />
@@ -25,5 +27,9 @@ const GenrePage = () => {
       </div>
     );
   };
+
+  function capitalizeFirstLetter(string) {
+    return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  }
 
   export default GenrePage;
