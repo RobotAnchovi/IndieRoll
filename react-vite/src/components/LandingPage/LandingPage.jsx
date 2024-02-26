ea23a796f174a9844723f93283a73884cd364195
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-02-25 22:29:17 -0500

    Update landing page with new features and routing

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -1,45 +1,50 @@
 // import { useEffect, useState } from "react";
-import "./LandingPage.css";
+import { useNavigate } from 'react-router-dom';
+import './LandingPage.css';
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+  const handleGetStarted = () => {
+    navigate('/signup');
+  };
   return (
-    <div className="landing-page">
-      <div className="heading-box">
-        <h1 className="landing-header">Where every stream counts</h1>
+    <div className='landing-page'>
+      <div className='heading-box'>
+        <h1 className='landing-header'>Where every stream counts</h1>
         <h2>Join the revolution of creators taking back their power</h2>
       </div>
-      <div className="all-features">
-        <div className="feature">
-          <div className="feature-info">
+      <div className='all-features'>
+        <div className='feature'>
+          <div className='feature-info'>
             <h2>Direct Profit to Creators</h2>
             <p>
-              At IndieRoll, we believe in the true value of creativity. That's
-              why we ensure 100% of the profits go directly to our creators.
-              Your work, your earnings. Enjoy the freedom of a platform where
-              every watch translates to direct income, supporting your passion
-              and hard work without intermediaries.
+              At IndieRoll, we believe in the true value of creativity.
+              That&apos;s why we ensure 100% of the profits go directly to our
+              creators. Your work, your earnings. Enjoy the freedom of a
+              platform where every watch translates to direct income, supporting
+              your passion and hard work without intermediaries.
             </p>
           </div>
 
-          <img className="feature-image" src="/feature-2.png" />
+          <img className='feature-image' src='/feature-2.png' />
         </div>
-        <div className="feature">
-          <div className="feature-info">
+        <div className='feature'>
+          <div className='feature-info'>
             <h2>Viewer-Driven Revenue Sharing</h2>
             <p>
               Your views have more power here. With a simple $5 monthly
               subscription, watchers become key players in supporting indie
               creators. Revenue is divided based on watch time, ensuring that
               your favorite creators are fairly rewarded for captivating your
-              attention. It's transparent, fair, and directly proportional to
-              the love each creation receives.
+              attention. It&apos;s transparent, fair, and directly proportional
+              to the love each creation receives.
             </p>
           </div>
 
-          <img className="feature-image" src="/feature-1.png" />
+          <img className='feature-image' src='/feature-1.png' />
         </div>
-        <div className="feature">
-          <div className="feature-info">
+        <div className='feature'>
+          <div className='feature-info'>
             <h2>Unlimited Access to Unique Content</h2>
             <p>
               Dive into a world where uniqueness thrives. IndieRoll offers an
@@ -50,12 +55,14 @@ const LandingPage = () => {
             </p>
           </div>
 
-          <img className="feature-image" src="/feature-4.png" />
+          <img className='feature-image' src='/feature-4.png' />
         </div>
       </div>
-      <footer className="landing-footer ">
+      <footer className='landing-footer '>
         Join IndieRoll Today: For the Love of Indie
-        <button className="get-started">Get Started</button>
+        <button className='get-started' onClick={handleGetStarted}>
+          Get Started
+        </button>
       </footer>
     </div>
   );
