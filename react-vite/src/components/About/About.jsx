f67898ba1cb472a432a6d499fbd7b7f220a9106c
Marisela Gomez <mariselaalejandragomez@gmail.com>
2024-03-11 17:09:43 -0700

    styled about page

--- react-vite/src/components/About/About.jsx
+++ react-vite/src/components/About/About.jsx
@@ -1,56 +1,72 @@
-import './About.css';
+import "./About.css";
 
 const AboutPage = () => {
-  return (
-    <div>
-      <h1>About IndieRoll</h1>
-      <p>
-        IndieRoll is a collaborative project developed by a dedicated team of
-        developers. It is a platform designed to showcase independent music,
-        allowing artists to share their work with a wider audience. Our project
-        leverages a modern tech stack to ensure a seamless user experience.
-      </p>
-
-      <h2>Technologies Used</h2>
-      <ul>
-        <li>Flask</li>
-        <li>Alembic</li>
-        <li>Postgres</li>
-        <li>React</li>
-        <li>Vite</li>
-        <li>NPM</li>
-        <li>Python</li>
-        <li>Redux</li>
-        <li>JavaScript</li>
-        <li>AWS</li>
-      </ul>
-
-      <h2>Contributors</h2>
-      <ul>
-        <li>
-          Jason Whitlock - <a href='https://github.com/RobotAnchovi'>GitHub</a>{' '}
-          | <a href='https://www.linkedin.com/in/whitlockjdev/'>LinkedIn</a>
-        </li>
-        <li>
-          Marisela Gomez - <a href='https://github.com/Mar1g0m3z'>GitHub</a> |{' '}
-          <a href='https://www.linkedin.com/in/marisela-g-881777231/'>
-            LinkedIn
-          </a>
-        </li>
-        <li>
-          Logan Fate - <a href='https://github.com/LoganFate'>GitHub</a> |{' '}
-          <a href='https://www.linkedin.com/in/logan-fate-9440402ba/'>
-            LinkedIn
-          </a>
-        </li>
-        <li>
-          Dwayne Walker -{' '}
-          <a href='https://github.com/worldofcreatives'>GitHub</a> |{' '}
-          <a href='https://www.linkedin.com/in/ipaintidraw'>LinkedIn</a>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<div className="about">
+				<h1 className="about-title">About IndieRoll</h1>
+				<p className="about-description">
+					IndieRoll is a collaborative project developed by a dedicated team of
+					developers. It is a platform designed to showcase independent music,
+					allowing artists to share their work with a wider audience. Our
+					project leverages a modern tech stack to ensure a seamless user
+					experience.
+				</p>
+			</div>
+			<div className="technologies">
+				<h2 className="technology-title">Technologies Used</h2>
+				<ul className="tech-list">
+					<li>Flask</li>
+					<li>Alembic</li>
+					<li>Postgres</li>
+					<li>React</li>
+					<li>Vite</li>
+					<li>NPM</li>
+					<li>Python</li>
+					<li>Redux</li>
+					<li>JavaScript</li>
+					<li>AWS</li>
+				</ul>
+			</div>
+			<h2 className="contributor-title">Contributors</h2>
+			<ul className="contributor-container">
+				<li className="contributor">
+					<div className="contributor-name">Jason Whitlock </div>
+					<div className="socials">
+						<a href="https://github.com/RobotAnchovi">GitHub</a>
+						<a href="https://www.linkedin.com/in/whitlockjdev/">
+							LinkedIn
+						</a>{" "}
+					</div>
+				</li>
+				<li className="contributor">
+					<div className="contributor-name">Marisela Gomez </div>
+					<div className="socials">
+						<a href="https://github.com/Mar1g0m3z">GitHub</a>
+						<a href="https://www.linkedin.com/in/marisela-g-881777231/">
+							LinkedIn
+						</a>
+					</div>
+				</li>
+				<li className="contributor">
+					<div className="contributor-name">Logan Fate </div>
+					<div className="socials">
+						<a href="https://github.com/LoganFate">GitHub</a>{" "}
+						<a href="https://www.linkedin.com/in/logan-fate-9440402ba/">
+							LinkedIn
+						</a>
+					</div>
+				</li>
+				<li className="contributor">
+					<div className="contributor-name">Dwayne Walker </div>
+					<div className="socials">
+						<a href="https://github.com/worldofcreatives">GitHub</a>{" "}
+						<a href="https://www.linkedin.com/in/ipaintidraw">LinkedIn</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default AboutPage;
